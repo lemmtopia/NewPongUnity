479d9dd4ec647ed10604ee68a3bdca9dcffa2192
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2025-07-08 11:39:25 -0300

    feat: Add random ball movement

--- Pong/Assets/Scripts/BallController.cs
+++ Pong/Assets/Scripts/BallController.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Vector2 myVelocity;
     [SerializeField] private float moveSpeed = 5f;
+
+    private Vector2 startVelocity;
 
     void Start()
     {
+        // Set a random velocity (if it is (0, 0) , default to (1, 1))
+        int xRandom = Random.Range(-1, 1);
+        int yRandom = Random.Range(-1, 1);
+        startVelocity.x = (xRandom != 0) ? (float)xRandom : 1f;
+        startVelocity.y = (yRandom != 0) ? (float)yRandom : 1f;
+
         // Set velocity to the left
-        rb.linearVelocity = myVelocity.normalized * moveSpeed;
+        rb.linearVelocity = startVelocity.normalized * moveSpeed;
     }
 
     void Update()
