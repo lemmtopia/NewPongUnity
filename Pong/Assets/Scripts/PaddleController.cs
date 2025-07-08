6712f96238897b5f4a2db8fd59b68a23a1012664
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2025-07-07 22:27:31 -0300

    feat: Add paddle movement

--- Pong/Assets/Scripts/PaddleController.cs
+++ Pong/Assets/Scripts/PaddleController.cs
@@ -2,16 +2,34 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float yBorder;
     [SerializeField] private float yPosition;
     private Vector3 myPosition;
 
     void Start()
     {
+        // Set myPosition to my starting position
         myPosition = transform.position;
     }
 
     void Update()
     {
+        // Get input from the user
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Move up
+            yPosition += moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            // Move down
+            yPosition -= moveSpeed * Time.deltaTime;
+        }
+
+        // Clamp my Y position
+        yPosition = Mathf.Clamp(yPosition, -yBorder, yBorder);
+
         // Changing my Y position
         myPosition.y = yPosition;
 
