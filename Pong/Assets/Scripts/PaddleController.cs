3bea9ddb3098d867056b7d511396119c2e2e11d4
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2025-07-08 11:31:10 -0300

    feat: Add player 2

--- Pong/Assets/Scripts/PaddleController.cs
+++ Pong/Assets/Scripts/PaddleController.cs
@@ -4,25 +4,41 @@ public class PaddleController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float yBorder;
+    [SerializeField] private bool isPlayer1;
 
     private float yPosition;
     private Vector3 myPosition;
+    
+    KeyCode keyUp;
+    KeyCode keyDown;
 
     void Start()
     {
         // Set myPosition to my starting position
         myPosition = transform.position;
+
+        // Keys
+        if (isPlayer1)
+        {
+            keyUp = KeyCode.W;
+            keyDown = KeyCode.S;
+        }
+        else
+        {
+            keyUp = KeyCode.UpArrow;
+            keyDown = KeyCode.DownArrow;
+        }
     }
 
     void Update()
     {
         // Get input from the user
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(keyUp))
         {
             // Move up
             yPosition += moveSpeed * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(keyDown))
         {
             // Move down
             yPosition -= moveSpeed * Time.deltaTime;
